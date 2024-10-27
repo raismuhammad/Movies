fc846f435f89645d4525a02c79f93ae25e23a473
raismuhammad <muhammadrais96@gmail.com>
2024-10-27 14:22:43 +0700

    add splash screen

--- app/src/main/java/com/rais/movies/ui/SplashActivity.kt
+++ app/src/main/java/com/rais/movies/ui/SplashActivity.kt
@@ -1,21 +1,38 @@
 package com.rais.movies.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.rais.movies.R
+import com.rais.movies.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
